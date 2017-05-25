a66c3c8bcfd88a5887b28f91576eb2f918bee79a
davinkevin <davin.kevin@gmail.com>
2017-05-25 15:19:02 +0200

    feat(book): this is the initial state to start the practice
    Goal to complete step 01-logic :
    * Have the book bind between the component and the view
    * Allow user to send a comment by filling the two inputs
    * Show each comments in a list (html template already defined)
    * Show the list of comments only if there are at least one comment
    * Each comment should contain login, text and creationDate (a js Date object)
    * The login should be print in uppercase
    * The creationDate should be at medium format

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'bs-root',
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Angular Formation Book Store works !';
+
+  book = {
+    title: 'Essential Angular',
+    description: 'The book is a short, but at the same time, fairly complete overview of the key aspects of Angular ' +
+    'written by its core contributors Victor Savkin and Jeff Cross, who are founders of Nrwl - Angular Consulting ' +
+    'for enterprise customers, from core contributors.',
+    coverUrl: 'assets/books/essential_angular.jpg'
+  };
+
 }
