896448bf6935cb3326880345304574af081b2f26
davinkevin <davin.kevin@gmail.com>
2017-06-03 19:55:21 +0200

    feat(http): load data from external api
    Goal to reach the next step 14-resolve-data-before-loading :
    * create resolver to load external data before component creation
    
    End of step 13-http-call #Http #JSON #json-server

--- src/app/books/shared/service/comment/comment.service.ts
+++ src/app/books/shared/service/comment/comment.service.ts
@@ -3,24 +3,34 @@ import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/delay';
+import {Http} from '@angular/http';
+import {Book} from '../book/book.service';
 
 export interface Comment {
   login: string;
   text: string;
   creationDate?: Date;
   note: number;
+  bookId?: number;
 }
 
 @Injectable()
 export class CommentService {
 
-  constructor() {}
+  constructor(private http: Http) {}
 
-  save(comment: Comment): Observable<Comment> {
+  save(comment: Comment, book: Book): Observable<Comment> {
     comment.creationDate = new Date();
-    return Observable.of(comment)
-        .delay(2000)
-        .map(c => Object.assign({}, c));
+
+    return this.http
+        .post(`/api/v1/books/${book.id}/comments`, comment)
+        .map(r => r.json());
+  }
+
+  findByBook(bookId: number): Observable<Array<Comment>> {
+    return this.http
+        .get(`/api/v1/books/${bookId}/comments`)
+        .map(r => r.json());
   }
 
 }
